a91c6a7b4e7f79fa8e10cca01901870f9b584462
DESKTOP-OPUO0GQ\mento <s1970403@s.do-johodai.ac.jp>
2023-01-22 23:05:39 +0900

    pdfアウトプット機能の作成

--- private_diary/diary/forms.py
+++ private_diary/diary/forms.py
@@ -5,25 +5,73 @@ from django.core.mail import EmailMessage
 
 class InquiryForm(forms.Form):
     name = forms.CharField(label='お名前', max_length=30)
+    '''
     email = forms.EmailField(label='メールアドレス')
+    address1 = forms.CharField(label='都道府県', max_length=30)
+    address2 = forms.CharField(label='市町村', max_length=30)
+    address3 = forms.CharField(label='市町村以下の住所', max_length=30)
+    tell_shigaikyokuban =  forms.CharField(label='市外局番', max_length=5)
+    year = forms.IntegerField(label='年',max_value=2023,min_value=1900)
+    month = forms.IntegerField(label='月',max_value=12,min_value=1)
+    day = forms.IntegerField(label='日',max_value=31,min_value=1)
+    genders = (
+        ("1", "男性"),
+        ("2", "女性"),
+    )
+    genders = forms.ChoiceField(label='性別',choices=genders)
+    #degree = forms.SmallIntegerField(label='体温')
+
+
     title = forms.CharField(label='タイトル', max_length=30)
     message = forms.CharField(label='メッセージ', widget=forms.Textarea)
+        '''
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.fields['name'].widget.attrs['class'] = 'form-control'
         self.fields['name'].widget.attrs['placeholder'] = 'お名前をここに入力してください。'
+        '''
         self.fields['email'].widget.attrs['class'] = 'form-control'
         self.fields['email'].widget.attrs['placeholder'] = 'メールアドレスをここに入力してください。'
         self.fields['title'].widget.attrs['class'] = 'form-control'
+        self.fields['address1'].widget.attrs['class'] = 'form-control'
+        self.fields['address1'].widget.attrs['placeholder'] = '都道府県名をここに入力してください。'
+        self.fields['address2'].widget.attrs['class'] = 'form-control'
+        self.fields['address2'].widget.attrs['placeholder'] = '市町村をここに入力してください。'
+        self.fields['address3'].widget.attrs['class'] = 'form-control'
+        self.fields['address3'].widget.attrs['placeholder'] = '市町村以下の住所をここに入力してください。'
         self.fields['title'].widget.attrs['placeholder'] = 'タイトルをここに入力してください。'
+        self.fields['tell_shigaikyokuban'].widget.attrs['class'] = 'form-control'
+        self.fields['tell_shigaikyokuban'].widget.attrs['placeholder'] = '電話番号をここに入力してください。'
+        self.fields['year'].widget.attrs['class'] = 'form-control'
+        self.fields['year'].widget.attrs['placeholder'] = 'お名前をここに入力してください。'
+        self.fields['month'].widget.attrs['class'] = 'form-control'
+        self.fields['month'].widget.attrs['placeholder'] = 'お名前をここに入力してください。'
+        self.fields['day'].widget.attrs['class'] = 'form-control'
+        self.fields['day'].widget.attrs['placeholder'] = 'お名前をここに入力してください。'
+        self.fields['genders'].widget.attrs['class'] = 'form-control'
+        self.fields['genders'].widget.attrs['placeholder'] = '性別をここに入力してください。'
+        #self.fields['degree'].widget.attrs['class'] = 'form-control'
+        #self.fields['degree'].widget.attrs['placeholder'] = '体温をここに入力してください。'
+
+
         self.fields['message'].widget.attrs['class'] = 'form-control'
         self.fields['message'].widget.attrs['placeholder'] = 'メッセージをここに入力してください。'
-
+'''
         def send_email(self):
             name = self.cleaned_data['name']
             email = self.cleaned_data['email']
+            address1 = self.cleaned_data['address1']
+            address2 = self.cleaned_data['address2']
+            address3 = self.cleaned_data['address3']
+            tell = self.cleaned_data['tell']
+            year = self.cleaned_data['year']
+            month = self.cleaned_data['month']
+            day = self.cleaned_data['day']
+            genders = self.cleaned_data['genders']
+            degree = self.cleaned_data['degree']
+
             title = self.cleaned_data['title']
             message = self.cleaned_data['message']
 
--- private_diary/diary/views.py
+++ private_diary/diary/views.py
@@ -1,13 +1,21 @@
+from pdfrw import PdfReader
+from pdfrw.buildxobj import pagexobj
+from pdfrw.toreportlab import makerl
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import portrait, landscape, A3, A4, A5, A6, B3, B4, B5, B6
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4, portrait
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
 import logging
 from django.urls import reverse_lazy
-from django.views import generic
 from django.contrib import messages
 
 from .forms import InquiryForm
 
 logger = logging.getLogger(__name__)
 
-
+from django.views import generic
 class IndexView(generic.TemplateView):
     template_name = "index.html"
 
@@ -16,7 +24,39 @@ class InquiryView(generic.FormView):
     form_class = InquiryForm
     success_url = reverse_lazy('diary:inquiry')
 
+
     def form_valid(self, form):
+
+        # フォントファイルを指定して、フォントを登録
+        fontname = "IPA Gothic"
+        pdfmetrics.registerFont(TTFont(fontname, './ipaexg.ttf'))
+
+        monshin_template = './monshin_template.pdf'
+        output_path = './monshin_output.pdf'
+
+        # 元のPDFを読み込み
+        pages = PdfReader(monshin_template, decompress=False).pages
+
+        # キャンバスのセット
+        cc = canvas.Canvas(output_path, pagesize=portrait(A4))
+
+        # ページ取得
+        pp = pagexobj(pages[0])
+        cc.doForm(makerl(cc, pp))
+
+        # 文字サイズで書き出し
+        cc.setFont(fontname, 15)  # フォントとサイズを指定
+        #cc.drawString(60, 755, '京都')  # x, y, 文字列を指定
+        cc.drawString(60, 755, form.cleaned_data['name'])  # x, y, 文字列を指定
+
+        # 丸を描画する
+        cc.circle(195, 755, 7, 1, 0)
+
+        cc.showPage()
+        cc.save()
+
+
+
         #form.send_email()
         print("aaaaaaaaaaaaaaaaaaaaa")
         messages.success(self.request,'メッセージを送信しました。')
