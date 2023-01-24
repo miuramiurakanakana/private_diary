99339ae726d68198dd8781a7a0bb7e24567027d7
DESKTOP-OPUO0GQ\mento <s1970403@s.do-johodai.ac.jp>
2023-01-24 11:23:47 +0900

    問診表更新

--- private_diary/diary/forms.py
+++ private_diary/diary/forms.py
@@ -4,14 +4,41 @@ from django import forms
 from django.core.mail import EmailMessage
 
 class InquiryForm(forms.Form):
-    name = forms.CharField(label='お名前', max_length=30)
+
     '''
+
+
     email = forms.EmailField(label='メールアドレス')
+
+    '''
     address1 = forms.CharField(label='都道府県', max_length=30)
     address2 = forms.CharField(label='市町村', max_length=30)
     address3 = forms.CharField(label='市町村以下の住所', max_length=30)
+    name = forms.CharField(label='お名前', max_length=30)
+    kana = forms.CharField(label='フリガナ', max_length=30)
+    tell_shigaikyokuban = forms.CharField(label='市外局番', max_length=5)
+    tell_shinaikyokuban = forms.CharField(label='市内局番', max_length=4)
+    tell_kanyuusyaban = forms.CharField(label='加入者番号', max_length=4)
+    birth = forms.CharField(label='誕生日', max_length=8)
+    yearsOld = forms.CharField(label='年齢', max_length=3)
+
+
+
+    times = forms.CharField(label='接種回数',max_length=2)
+    lastTime1 = forms.CharField(label='前回の摂取年',max_length=4)
+    lastTime2 = forms.CharField(label='前回の摂取月', max_length=2)
+    lastTime3 = forms.CharField(label='前回の摂取日', max_length=2)
+    kind = forms.CharField(label='前回のワクチンの種類', max_length=30)
+
+    sick = forms.CharField(label='病名',max_length=30)
+    badCondition = forms.CharField(label='具合悪い点',max_length=30)
+    allegy = forms.CharField(label='アレルギー',max_length=30)
+    sessyu1 = forms.CharField(label='具合が悪くなった予防接種名',max_length=30)
+    symptoms = forms.CharField(label='症状',max_length=30)
+    sessyu2 = forms.CharField(label='前回受けた予防接種名', max_length=30)
+    day = forms.CharField(label='前回受けた予防接種日', max_length=6)
+    '''
     tell_shigaikyokuban =  forms.CharField(label='市外局番', max_length=5)
-    year = forms.IntegerField(label='年',max_value=2023,min_value=1900)
     month = forms.IntegerField(label='月',max_value=12,min_value=1)
     day = forms.IntegerField(label='日',max_value=31,min_value=1)
     genders = (
@@ -24,28 +51,66 @@ class InquiryForm(forms.Form):
 
     title = forms.CharField(label='タイトル', max_length=30)
     message = forms.CharField(label='メッセージ', widget=forms.Textarea)
-        '''
+    '''
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
+        '''
         self.fields['name'].widget.attrs['class'] = 'form-control'
         self.fields['name'].widget.attrs['placeholder'] = 'お名前をここに入力してください。'
-        '''
         self.fields['email'].widget.attrs['class'] = 'form-control'
         self.fields['email'].widget.attrs['placeholder'] = 'メールアドレスをここに入力してください。'
         self.fields['title'].widget.attrs['class'] = 'form-control'
+        '''
         self.fields['address1'].widget.attrs['class'] = 'form-control'
         self.fields['address1'].widget.attrs['placeholder'] = '都道府県名をここに入力してください。'
         self.fields['address2'].widget.attrs['class'] = 'form-control'
         self.fields['address2'].widget.attrs['placeholder'] = '市町村をここに入力してください。'
         self.fields['address3'].widget.attrs['class'] = 'form-control'
         self.fields['address3'].widget.attrs['placeholder'] = '市町村以下の住所をここに入力してください。'
+        self.fields['name'].widget.attrs['class'] = 'form-control'
+        self.fields['name'].widget.attrs['placeholder'] = 'お名前をここに入力してください。'
+        self.fields['kana'].widget.attrs['class'] = 'form-control'
+        self.fields['kana'].widget.attrs['placeholder'] = 'お名前をここに入力してください。'
+        self.fields['tell_shigaikyokuban'].widget.attrs['class'] = 'form-control'
+        self.fields['tell_shigaikyokuban'].widget.attrs['placeholder'] = '市外局番をここに入力してください。'
+        self.fields['tell_shinaikyokuban'].widget.attrs['class'] = 'form-control'
+        self.fields['tell_shinaikyokuban'].widget.attrs['placeholder'] = '市内局番をここに入力してください。'
+        self.fields['tell_kanyuusyaban'].widget.attrs['class'] = 'form-control'
+        self.fields['tell_kanyuusyaban'].widget.attrs['placeholder'] = '加入者番号をここに入力してください。'
+        self.fields['birth'].widget.attrs['class'] = 'form-control'
+        self.fields['birth'].widget.attrs['placeholder'] = '誕生日をここに入力してください。'
+
+
+        self.fields['times'].widget.attrs['class'] = 'form-control'
+        self.fields['times'].widget.attrs['placeholder'] = 'ワクチンの接種回数をここに入力してください。'
+        self.fields['lastTime1'].widget.attrs['class'] = 'form-control'
+        self.fields['lastTime1'].widget.attrs['placeholder'] = '前回のワクチン接種年をここに入力してください。'
+        self.fields['lastTime2'].widget.attrs['class'] = 'form-control'
+        self.fields['lastTime2'].widget.attrs['placeholder'] = '前回のワクチンの接種月をここに入力してください。'
+        self.fields['lastTime3'].widget.attrs['class'] = 'form-control'
+        self.fields['lastTime3'].widget.attrs['placeholder'] = '前回ワクチンの接種日をここに入力してください。'
+
+        self.fields['sick'].widget.attrs['class'] = 'form-control'
+        self.fields['sick'].widget.attrs['placeholder'] = '病名をここに入力してください。'
+        self.fields['badCondition'].widget.attrs['class'] = 'form-control'
+        self.fields['badCondition'].widget.attrs['placeholder'] = '具合悪い点をここに入力してください。'
+        self.fields['allegy'].widget.attrs['class'] = 'form-control'
+        self.fields['allegy'].widget.attrs['placeholder'] = 'アレルギーをここに入力してください'
+        self.fields['kind'].widget.attrs['class'] = 'form-control'
+        self.fields['kind'].widget.attrs['placeholder'] = '接種したワクチンの種類をここに入力してください'
+        self.fields['sessyu1'].widget.attrs['class'] = 'form-control'
+        self.fields['sessyu1'].widget.attrs['placeholder'] = 'アレルギーのある予防接種名をここに入力してください'
+        self.fields['symptoms'].widget.attrs['class'] = 'form-control'
+        self.fields['symptoms'].widget.attrs['placeholder'] = '症状をここに入力してください'
+        self.fields['sessyu2'].widget.attrs['class'] = 'form-control'
+        self.fields['sessyu2'].widget.attrs['placeholder'] = '前回受けた予防接種名をここに入力してください'
+        self.fields['day'].widget.attrs['class'] = 'form-control'
+        self.fields['day'].widget.attrs['placeholder'] = '前回受けた予防接種日をここに入力してください'
+        '''
         self.fields['title'].widget.attrs['placeholder'] = 'タイトルをここに入力してください。'
         self.fields['tell_shigaikyokuban'].widget.attrs['class'] = 'form-control'
         self.fields['tell_shigaikyokuban'].widget.attrs['placeholder'] = '電話番号をここに入力してください。'
-        self.fields['year'].widget.attrs['class'] = 'form-control'
-        self.fields['year'].widget.attrs['placeholder'] = 'お名前をここに入力してください。'
         self.fields['month'].widget.attrs['class'] = 'form-control'
         self.fields['month'].widget.attrs['placeholder'] = 'お名前をここに入力してください。'
         self.fields['day'].widget.attrs['class'] = 'form-control'
@@ -58,8 +123,13 @@ class InquiryForm(forms.Form):
 
         self.fields['message'].widget.attrs['class'] = 'form-control'
         self.fields['message'].widget.attrs['placeholder'] = 'メッセージをここに入力してください。'
-'''
+        '''
+
+
+
+
         def send_email(self):
+            '''
             name = self.cleaned_data['name']
             email = self.cleaned_data['email']
             address1 = self.cleaned_data['address1']
@@ -74,7 +144,6 @@ class InquiryForm(forms.Form):
 
             title = self.cleaned_data['title']
             message = self.cleaned_data['message']
-
             subject = 'お問い合わせ{}'.format(title)
             message = '送信者名：{0}\nメールアドレス：{1}\nメッセージ：\n{2}'.format(name, email, message)
             from_email = os.environ.get('FROM_EMAIL')
@@ -87,5 +156,6 @@ class InquiryForm(forms.Form):
 
             message = EmailMessage(subject=subject,body=message,from_email=from_email,to=to_list,cc=cc_list)
             message.send()
+            '''
 
 
--- private_diary/diary/views.py
+++ private_diary/diary/views.py
@@ -45,9 +45,50 @@ class InquiryView(generic.FormView):
         cc.doForm(makerl(cc, pp))
 
         # 文字サイズで書き出し
+        cc.setFont(fontname, 20)  # フォントとサイズを指定
+        birdday = form.cleaned_data['birth']
+        cc.drawString(64, 664, birdday[0])  # x, y, 文字列を指定
+        cc.drawString(81, 664, birdday[1])  # x, y, 文字列を指定
+        cc.drawString(98, 664, birdday[2])  # x, y, 文字列を指定
+        cc.drawString(115, 664, birdday[3])  # x, y, 文字列を指定
+        cc.drawString(143, 664, birdday[4])  # x, y, 文字列を指定
+        cc.drawString(160, 664, birdday[5])  # x, y, 文字列を指定
+        cc.drawString(188, 664, birdday[6])  # x, y, 文字列を指定
+        cc.drawString(205, 664, birdday[7])  # x, y, 文字列を指定
+
+
+        # 文字サイズで書き出し
         cc.setFont(fontname, 15)  # フォントとサイズを指定
+
         #cc.drawString(60, 755, '京都')  # x, y, 文字列を指定
-        cc.drawString(60, 755, form.cleaned_data['name'])  # x, y, 文字列を指定
+        cc.drawString(60, 755, form.cleaned_data['address1'] + "✓")  # x, y, 文字列を指定
+        cc.drawString(230, 755, form.cleaned_data['address2'])  # x, y, 文字列を指定
+        cc.drawString(60, 728, form.cleaned_data['address3'])  # x, y, 文字列を指定
+        cc.drawString(60, 692, form.cleaned_data['name'])  # x, y, 文字列を指定
+        cc.drawString(105, 615, form.cleaned_data['times'])  # x, y, 文字列を指定
+        cc.drawString(245, 615, form.cleaned_data['lastTime1'])  # x, y, 文字列を指定
+        cc.drawString(316, 615, form.cleaned_data['lastTime2'])  # x, y, 文字列を指定
+        cc.drawString(364, 615, form.cleaned_data['lastTime3'])  # x, y, 文字列を指定
+
+
+        cc.setFont(fontname, 10)  # フォントのサイズを指定
+        cc.drawString(300, 460, form.cleaned_data['sick'])  # x, y, 文字列を指定
+        cc.drawString(240, 440, form.cleaned_data['badCondition'])  # x, y, 文字列を指定
+        cc.drawString(170, 390, form.cleaned_data['allegy'])  # x, y, 文字列を指定
+        cc.drawString(235, 603, form.cleaned_data['kind'])  # x, y, 文字列を指定
+        cc.drawString(70, 361, form.cleaned_data['sessyu1'])  # x, y, 文字列を指定
+        cc.drawString(280, 361, form.cleaned_data['symptoms'])  # x, y, 文字列を指定
+        cc.drawString(225, 324, form.cleaned_data['sessyu2'])  # x, y, 文字列を指定
+        cc.drawString(370, 324, form.cleaned_data['day'])  # x, y, 文字列を指定
+
+        cc.setFont(fontname, 7)  # フォントのサイズを指定
+        # cc.drawString(60, 755, '京都')
+        # x, y, 文字列を指定
+        cc.drawString(60, 712, form.cleaned_data['kana'])  # x, y, 文字列を指定
+        cc.drawString(280, 708, form.cleaned_data['tell_shigaikyokuban'])   # x, y, 文字列を指定
+        cc.drawString(280, 692, form.cleaned_data['tell_shinaikyokuban'])   # x, y, 文字列を指定
+        cc.drawString(328, 692, form.cleaned_data['tell_kanyuusyaban'])  # x, y, 文字列を指定
+
 
         # 丸を描画する
         cc.circle(195, 755, 7, 1, 0)
@@ -55,11 +96,4 @@ class InquiryView(generic.FormView):
         cc.showPage()
         cc.save()
 
-
-
-        #form.send_email()
-        print("aaaaaaaaaaaaaaaaaaaaa")
-        messages.success(self.request,'メッセージを送信しました。')
-        logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
-        print(form.cleaned_data['name'])
         return super().form_valid(form)
